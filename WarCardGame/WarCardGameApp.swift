e2edc3bdb93e80ca00505b42c224c365f604024b
narutoben10af <limsieian@me.com>
2021-10-16 00:04:55 +0900

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Lim Si Eian on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
